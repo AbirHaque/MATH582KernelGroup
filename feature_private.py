e2bdaa461a63cde4f46368eba91bbb1facea84e9
Manoj Turaga <manoj.turaga@ku.edu>
2023-10-31 13:51:16 -0500

    Feature: Initial commits to Feature private module

---
+++ feature_private.py
@@ -0,0 +1,25 @@
+"""
+Math 582 - Private Module for Feature Selection
+"""
+
+class FeatureSelection:
+    def __init__( self, model, dataframe ):
+        self._model = model
+        self._dataframe = dataframe
+        self._mutation_buffer = []
+
+    def _create_features( self ):
+        pass
+
+    def _pca( self ):
+        pass
+
+    def __create_mutations( self ):
+        pass
+
+    def _genetic_algorithm( self ):
+        pass
+
+    def _get_best_model( self ):
+        pass
+        
